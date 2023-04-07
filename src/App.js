8610e6cc6d2aba944a766dd1e753ac8b230a5977
AnupamRawal <rawalanupam5@gmail.com>
2023-04-07 14:52:07 +0530

    video player added and coco-ssd modal loaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 
 function App() {
+  const videoRef = useRef(null);
+
+  // Load the COCO-SSD model
+  const loadModel = async () => {
+    const net = await cocossd.load();
+    return net;
+  };
+
+  // Run the object detection on the uploaded video
+  const runDetection = async (net) => {
+    if (videoRef.current !== null) {
+      const video = videoRef.current;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+      video.width = videoWidth;
+      video.height = videoHeight;
+
+      // Start the video playback and wait for it to start
+      await video.play();
+    }
+  };
+
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    const video = videoRef.current;
+    const source = URL.createObjectURL(file);
+    video.src = source;
+
+    const net = await loadModel();
+    runDetection(net);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={handleFileChange} />
+      <div>
+        <video ref={videoRef}/>
+      </div>
     </div>
   );
 }
